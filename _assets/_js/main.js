23e5f292a00c7c9198c855efee393f83b49b392a
Karan Khanchandani <karan.khanchandani94@gmail.com>
2017-03-23 20:04:53 +0530

    Minimum Animation
    Adds Minimum Animation to the game.

--- _assets/_js/main.js
+++ _assets/_js/main.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
+
+	var sky = $(".sky"),
+    tl;
+	var balls = $('<div class = "ball"/>'); 
+	console.log(balls);
+	console.log(sky);
+	sky.append(balls);
+	TweenMax.set(balls, {x:500, y:480})
+
+
+
+
 	$(".sky").click(function(event){
 	var audio = document.getElementById("audio");
 	audio.load();
 	audio.play();
 });
 });
+
+
